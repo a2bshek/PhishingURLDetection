911d06b9e2ecc8acf63fb394c0b8a0583684457e
Abishek <abishek292001@gmail.com>
2025-08-09 21:41:25 +0530

    Removing unnecessary files from the repository

--- extension/server/predictor.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from feature_extraction import generate_data_set
-import pickle
-import sys
-model = pickle.load(open("./xgb.pkl","rb"))
-#print("python script output:"+sys.argv[1])
-x = model.predict([generate_data_set(sys.argv[1])])
-print(x)
